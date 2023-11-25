b64df7d4d4091e4d3860d020f0a167db37872f3d
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-25 21:25:58 +0700

    fix sign up button text

--- src/app/ui/signUp/SignUpForm.jsx
+++ src/app/ui/signUp/SignUpForm.jsx
@@ -63,7 +63,7 @@ const SignUpForm = () => {
           </ul>
         </div>
       </div>
-      <button className='px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium transition duration-150'>Sign In</button>
+      <button className='px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium transition duration-150'>Sign Up</button>
     </form>
   )
 };
