e191a7841b83c6128579d7182bbb8304d0d16145
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-25 21:08:17 +0700

    adjust sign up page mobile view

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -5,13 +5,13 @@ import SignUpForm from '../ui/signUp/SignUpForm';
 const SignUpPage = () => {
   return (
     <main className="flex flex-col lg:flex-row h-screen">
-      <div className='basis-1/2 lg:basis-1/2 flex justify-center items-center'>
+      <div className='basis-1/2 md:basis-1/2 flex justify-center items-center'>
         <div className='flex flex-col'>
           <h1 className='font-extrabold text-6xl md:text-8xl'>Post-It</h1>
           <p className='ps-1 font-medium text-sm md:text-base'>Post your thoughts freely.</p>
         </div>
       </div>
-      <div className='basis-1/2 lg:basis-1/2 bg-slate-100 py-8 flex items-start lg:items-center overflow-auto'>
+      <div className='basis-1/2 md:basis-1/2 bg-slate-100 py-8 flex items-start lg:items-center overflow-auto'>
         <div className='flex flex-col px-8 md:px-0 gap-2 w-full md:w-2/3 mx-auto'>
           <SignUpForm/>
           <p>Already have an account? <Link href="/" className='text-blue-500'>Sign In</Link> now.</p>
