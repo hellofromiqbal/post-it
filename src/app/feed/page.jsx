d5c5e037981f45ae877f44e959909ea22df68204
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-26 18:00:46 +0700

    add create post form component

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
+import CreatePostForm from '../ui/feed/CreatePostForm';
 
 const FeedPage = () => {
   return (
     <main className='max-w-6xl mx-auto min-h-screen flex flex-col gap-4'>
       <div className='bg-softDark text-light shadow-md flex flex-col rounded-md p-4'>
-        <form action="" className='flex flex-col gap-4'>
-          <input
-            type="text"
-            placeholder='Post title'
-            className='px-4 py-2 bg-transparent border rounded-md'
-          />
-          <textarea
-            cols="30"
-            rows="10"
-            placeholder='Post description'
-            className='px-4 py-2 bg-transparent border rounded-md resize-none'
-          ></textarea>
-          <button className='px-4 py-2 bg-green-500 hover:bg-green-400 text-black font-semibold rounded-full transition duration-150'>Post</button>
-        </form>
+        <CreatePostForm/>
       </div>
     </main>
   )
---
+++ src/app/ui/feed/CreatePostForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CreatePostForm = () => {
+  return (
+    <form action="" className='flex flex-col gap-4'>
+      <input
+        type="text"
+        placeholder='Post title'
+        className='px-4 py-2 bg-transparent border rounded-md'
+      />
+      <textarea
+        cols="30"
+        rows="10"
+        placeholder='Post description'
+        className='px-4 py-2 bg-transparent border rounded-md resize-none'
+      ></textarea>
+      <button className='px-4 py-2 bg-green-500 hover:bg-green-400 text-black font-semibold rounded-full transition duration-150'>Post</button>
+    </form>
+  )
+};
+
+export default CreatePostForm;
