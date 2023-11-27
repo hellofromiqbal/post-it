fd83ca334b79b724806e9f6930bb0b1e22e9ee7f
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-27 11:22:31 +0700

    adjust create post form component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,10 +3,8 @@ import CreatePostForm from '../ui/dashboard/CreatePostForm';
 
 const FeedPage = () => {
   return (
-    <div className='max-w-6xl mx-auto min-h-screen flex flex-col gap-4'>
-      <div className='bg-softDark text-light shadow-md flex flex-col rounded-md p-4'>
-        <CreatePostForm/>
-      </div>
+    <div className='flex flex-col gap-4'>
+      <CreatePostForm/>
     </div>
   )
 };
--- src/app/ui/dashboard/CreatePostForm.jsx
+++ src/app/ui/dashboard/CreatePostForm.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 const CreatePostForm = () => {
   return (
-    <form action="" className='flex flex-col gap-4'>
-      {/* <input
-        type="text"
-        placeholder='Post title'
-        className='px-4 py-2 bg-transparent border border-light rounded-md'
-      /> */}
+    <form action="" className='bg-softDark text-light shadow-md rounded-md flex flex-col gap-4 p-4'>
       <textarea
         cols="30"
         rows="3"
