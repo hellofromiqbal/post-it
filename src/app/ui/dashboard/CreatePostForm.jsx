ff46245299ba1e3867230aab231f8e2d44f2c74c
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-27 11:18:53 +0700

    adjust create post form

--- src/app/ui/dashboard/CreatePostForm.jsx
+++ src/app/ui/dashboard/CreatePostForm.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const CreatePostForm = () => {
   return (
     <form action="" className='flex flex-col gap-4'>
-      <input
+      {/* <input
         type="text"
         placeholder='Post title'
         className='px-4 py-2 bg-transparent border border-light rounded-md'
-      />
+      /> */}
       <textarea
         cols="30"
-        rows="10"
-        placeholder='Post description'
+        rows="3"
+        placeholder='What is happening?!'
         className='px-4 py-2 bg-transparent border border-light rounded-md resize-none'
       ></textarea>
       <button className='px-4 py-2 bg-green-500 hover:bg-green-600 text-black font-semibold rounded-full transition duration-150'>Post</button>
