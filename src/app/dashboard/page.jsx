bd9f27cf552df79ad4a4196c16dbc59e09dc9fd5
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-27 13:06:20 +0700

    add feeds component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CreatePostForm from '../ui/dashboard/CreatePostForm';
+import Feeds from '../ui/dashboard/feeds';
 
 const FeedPage = () => {
   return (
     <div className='flex flex-col gap-4'>
       <CreatePostForm/>
+      <Feeds/>
     </div>
   )
 };
---
+++ src/app/ui/dashboard/Feeds.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from 'react';
+
+const Feeds = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='bg-softDark text-light shadow-md rounded-md flex flex-col p-4'>
+        <h2 className='font-medium'>Username</h2>
+        <p className='opacity-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, vero.</p>
+      </div>
+    </div>
+  )
+};
+
+export default Feeds;
