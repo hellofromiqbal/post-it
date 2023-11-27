13f7ad487215c072c8bd6ad54633bc4c5a8a4565
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-27 16:02:35 +0700

    adjust feed card

--- src/app/ui/dashboard/Feeds.jsx
+++ src/app/ui/dashboard/Feeds.jsx
@@ -1,7 +1,10 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
+import { FaHeart, FaShare } from "react-icons/fa6";
+import { FaComment } from "react-icons/fa";
 
 const Feeds = () => {
   const [posts, setPosts] = useState([]);
@@ -22,9 +25,25 @@ const Feeds = () => {
               <Image src="https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWFufGVufDB8fDB8fHww" alt='profpic' fill className='object-cover'/>
             </div>
           </div>
-          <div className='flex flex-col'>
-            <h2 className='font-semibold'>@pedromachado</h2>
-            <p className='opacity-70'>Only when I change my browser Tab and then change Tab into my application again the client-side code is getting fetched and executed.</p>
+          <div className='flex flex-col gap-2'>
+            <div className='flex flex-col'>
+              <h2 className='font-semibold'>@pedromachado</h2>
+              <p className='opacity-70'>Only when I change my browser Tab and then change Tab into my application again the client-side code is getting fetched and executed.</p>
+            </div>
+            <div className='flex justify-end items-center gap-10 text-light'>
+              <Link href="#" className='flex gap-2 items-center'>
+                <FaHeart size={20}/>
+                <span className='text-sm opacity-70'>1.6K</span>
+              </Link>
+              <Link href="#" className='flex gap-2 items-center'>
+                <FaComment size={20}/>
+                <span className='text-sm opacity-70'>756</span>
+              </Link>
+              <Link href="#" className='flex gap-2 items-center'>
+                <FaShare size={20}/>
+                <span className='text-sm opacity-70'>800</span>
+              </Link>
+            </div>
           </div>
         </div>
       ))}
