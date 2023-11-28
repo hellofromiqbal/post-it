a7a08b854e5117dbcf176c3e27402ead02642991
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-28 21:32:40 +0700

    adjust right side

--- src/app/ui/dashboard/TrendingList.jsx
+++ src/app/ui/dashboard/TrendingList.jsx
@@ -5,28 +5,33 @@ import Link from 'next/link';
 
 const TrendingList = () => {
   return (
-    <ul className='bg-softDark shadow-md rounded-md overflow-hidden flex flex-col'>
-      <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-        <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-        <h3 className='text-lg font-bold'>Free Palestine</h3>
-        <small className='text-xs font-semibold opacity-70'>31.3K posts</small>
-      </Link>
-      <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-        <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-        <h3 className='text-lg font-bold'>Stop Genocide</h3>
-        <small className='text-xs font-semibold opacity-70'>29.1K posts</small>
-      </Link>
-      <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-        <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-        <h3 className='text-lg font-bold'>Premier League</h3>
-        <small className='text-xs font-semibold opacity-70'>20.7K posts</small>
-      </Link>
-      <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-        <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-        <h3 className='text-lg font-bold'>Winter Is Coming</h3>
-        <small className='text-xs font-semibold opacity-70'>17.3K posts</small>
-      </Link>
-    </ul>
+    <div className='bg-softDark shadow-md rounded-md text-light overflow-hidden flex flex-col'>
+      <div className='px-4 py-2'>
+        <h2 className='font-bold text-xl'>Hot Topic</h2>
+      </div>
+      <ul className='flex flex-col'>
+        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
+          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
+          <h3 className='text-lg font-bold'>Free Palestine</h3>
+          <small className='text-xs font-semibold opacity-70'>31.3K posts</small>
+        </Link>
+        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
+          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
+          <h3 className='text-lg font-bold'>Stop Genocide</h3>
+          <small className='text-xs font-semibold opacity-70'>29.1K posts</small>
+        </Link>
+        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
+          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
+          <h3 className='text-lg font-bold'>Premier League</h3>
+          <small className='text-xs font-semibold opacity-70'>20.7K posts</small>
+        </Link>
+        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
+          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
+          <h3 className='text-lg font-bold'>Winter Is Coming</h3>
+          <small className='text-xs font-semibold opacity-70'>17.3K posts</small>
+        </Link>
+      </ul>
+    </div>
   )
 };
 
