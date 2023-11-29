ce7c21542f2e10f01985f3430c053c85ffdc3a24
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-29 15:15:44 +0700

    add current user provider

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,26 +1,12 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { saveCurrentUserDetails } from '@/store/currentUserSlicer';
 import CreatePostForm from '../ui/dashboard/CreatePostForm';
 import Feeds from '../ui/dashboard/Feeds';
 
 const FeedPage = () => {
-  const currentUser = useSelector((state) => state);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    fetch('/api/users/profile', { cache: 'no-store' })
-      .then(res => res.json())
-      .then(data => dispatch(saveCurrentUserDetails(data.data)))
-      .catch(err => console.log(err.message));
-  }, []);
-
   return (
     <div className='flex flex-col gap-4'>
-      {console.log(currentUser)}
       <CreatePostForm/>
       <Feeds/>
     </div>
