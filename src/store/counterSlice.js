a232ce4dec59bf6a671a16de8982729d52f29d16
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-11-29 13:18:27 +0700

    adjust counterslicer

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -1,20 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {
-  value: 0
-};
+const initialState = {};
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    fetchUserData: (state, action) => {
+      state.value = action.payload.email;
     }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { fetchUserData } = counterSlice.actions;
 
 export default counterSlice.reducer
