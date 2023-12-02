b5dfb63f3372bc2997c4f5162845eb64eaff4e0c
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-02 20:57:44 +0700

    use react-redux to store posts data

--- src/app/ui/dashboard/Feeds.jsx
+++ src/app/ui/dashboard/Feeds.jsx
@@ -3,20 +3,22 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import ContentCard from './ContentCard';
+import { useSelector } from 'react-redux';
 
 const Feeds = () => {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    fetch("/api/posts/", { cache: 'no-store' })
-      .then(res => res.json())
-      .then(data => setPosts(data.data))
-      .catch(err => console.log(err.message));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/posts/", { cache: 'no-store' })
+  //     .then(res => res.json())
+  //     .then(data => setPosts(data.data))
+  //     .catch(err => console.log(err.message));
+  // }, []);
+  const posts = useSelector(state => state.currentPosts.value);
 
   return (
     <div className='bg-softDark rounded-md shadow-md text-light overflow-hidden'>
-      {posts.map(post => (
+      {posts?.map(post => (
         <Link key={post._id} href={`/dashboard/posts/${post._id}`}>
           <ContentCard data={post}/>
         </Link>
