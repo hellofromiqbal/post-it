b2761d097ed039d24261ccf4c7bb84854e3d259b
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-03 12:26:04 +0700

    adjust like button icon

--- src/app/ui/dashboard/LikeButton.jsx
+++ src/app/ui/dashboard/LikeButton.jsx
@@ -51,9 +51,9 @@ const LikeButton = ({ id, contentType }) => {
   return (
     <button className='flex gap-2 items-center' onClick={(e) => handleLike(e)}>
       {isLikedByCurrentUser ?
-        <FaHeart size={contentType === 'post' ? 20 : 15}/>
+        <FaHeart size={contentType === 'post' ? 20 : 15} className='text-green-500'/>
         :
-        <FaRegHeart size={contentType === 'post' ? 20 : 15}/>
+        <FaHeart size={contentType === 'post' ? 20 : 15}/>
       }
       <span className={`${contentType === 'post' ? 'text-sm' : ''} opacity-70`}>{likesCount?.length}</span>
     </button>
