803e5acb383190674fed33d3a2405e8aa546063e
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-03 17:07:42 +0700

    adjust navbar bg using pathname

--- src/app/ui/dashboard/Navbar.jsx
+++ src/app/ui/dashboard/Navbar.jsx
@@ -13,32 +13,34 @@ import {
 import { FaUserCircle } from "react-icons/fa";
 import SignOutButton from './SignOutButton';
 import { useSelector } from 'react-redux';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
   const currentUser = useSelector(state => state.currentUser.value);
   return (
     <nav className='flex flex-col bg-softDark shadow-md rounded-md overflow-hidden'>
-      <Link href="/dashboard/" className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href="/dashboard/" className={`${pathname === "/dashboard" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <IoHome size={25}/>
         <span>Home</span>
       </Link>
-      <Link href="/dashboard/explore" className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href="/dashboard/explore" className={`${pathname === "/dashboard/explore" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <IoSearch size={25}/>
         <span>Explore</span>
       </Link>
-      <Link href="/dashboard/notifications" className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href="/dashboard/notifications" className={`${pathname === "/dashboard/notifications" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <IoNotifications size={25}/>
         <span>Notifications</span>
       </Link>
-      <Link href="/dashboard/messages" className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href="/dashboard/messages" className={`${pathname === "/dashboard/messages" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <IoMail size={25}/>
         <span>Messages</span>
       </Link>
-      <Link href="/dashboard/friends" className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href="/dashboard/friends" className={`${pathname === "/dashboard/friends" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <IoPeople size={25}/>
         <span>Friends</span>
       </Link>
-      <Link href={`/dashboard/profile/${currentUser?.username}`} className='text-light hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4'>
+      <Link href={`/dashboard/profile/${currentUser?.username}`} className={`${pathname.includes(currentUser?.username) ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex items-center gap-4`}>
         <FaUserCircle size={25}/>
         <span>Profile</span>
       </Link>
