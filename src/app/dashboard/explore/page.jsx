a9b21003e78c1c21fc037a5ce7c1e7f0e3cee97b
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-04 11:11:21 +0700

    add explore, notifications, messages & friends page

---
+++ src/app/dashboard/explore/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ExplorePage = () => {
+  return (
+    <div className='bg-softDark rounded-md shadow-md text-light flex flex-col gap-0 overflow-hidden'>
+      <div className='flex flex-col p-4'>
+        <h1 className='text-xl font-semibold'>Explore</h1>
+        <p className='opacity-70'>Comming soon!</p>
+      </div>
+    </div>
+  )
+};
+
+export default ExplorePage;
