69ae561c7622807940c56f50adbbddd30c6583f2
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-07 12:18:00 +0700

    adjust currentPostSlicer

--- src/store/currentPostsSlicer.js
+++ src/store/currentPostsSlicer.js
@@ -18,14 +18,28 @@ export const currentPostsSlicer = createSlice({
       state.value = state.value.filter(item => item._id !== action.payload);
     },
     updatePost: (state, action) => {
-      const filteredStateValue = state.value.filter(item => item.authorId !== action.payload.authorId);
-      const currentUser = state.value.find(item => item.authorId === action.payload.authorId);
-      const updatedUser = {
-        ...currentUser,
-        authorFullname: action.payload.authorFullname,
-        authorUsername: action.payload.authorUsername
-      };
-      state.value = [updatedUser, ...filteredStateValue];
+      // const filteredStateValue = state.value.filter(item => item.authorId !== action.payload.authorId);
+      // const currentUser = state.value.find(item => item.authorId === action.payload.authorId);
+      // const updatedUser = {
+      //   ...currentUser,
+      //   authorFullname: action.payload.authorFullname,
+      //   authorUsername: action.payload.authorUsername
+      // };
+      // state.value = [updatedUser, ...filteredStateValue];
+      // const filteredStateValue = state.value.filter(item => item.authorId === action.payload.authorId);
+      // console.log(filteredStateValue);
+      state.value = state.value.map(item => {
+        if(item.authorId === action.payload.authorId) {
+          return {
+            ...item,
+            authorFullname: action.payload.authorFullname,
+            authorUsername: action.payload.authorUsername
+          }
+        } else {
+          return item;
+        };
+      });
+      console.log(state.value);
     }
   },
 })
