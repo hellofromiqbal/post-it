54264038030ece964bc0a406644c626da27b3040
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-09 14:46:54 +0700

    adjust delete account button ui

--- src/app/ui/dashboard/DeleteAccountButton.jsx
+++ src/app/ui/dashboard/DeleteAccountButton.jsx
@@ -20,7 +20,7 @@ const DeleteAccountButton = ({id}) => {
 
   return (
     <button
-      className='px-4 py-2 bg-red-500 hover:bg-red-600 text-black font-semibold rounded-full transition duration-150'
+      className='px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-full transition duration-150'
       onClick={(e) => handleDelete(e)}
     >Delete Account</button>
   )
--- src/app/ui/dashboard/EditProfileForm.jsx
+++ src/app/ui/dashboard/EditProfileForm.jsx
@@ -103,7 +103,7 @@ const EditProfileForm = () => {
           {...register("website")}
         ></input>
       </div>
-      <div className='flex flex-col gap-1'>
+      <div className='flex flex-col gap-4'>
         <button className='px-4 py-2 bg-green-500 hover:bg-green-600 text-black font-semibold rounded-full transition duration-150'>Save Changes</button>
         <DeleteAccountButton id={currentUser?._id}/>
       </div>
