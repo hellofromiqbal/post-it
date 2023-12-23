93c645338e1c65a4870a5544e54abf95cb06fc7f
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-23 12:18:41 +0700

    adjust navbar order on mobile viewport

--- src/app/ui/dashboard/Navbar.jsx
+++ src/app/ui/dashboard/Navbar.jsx
@@ -19,23 +19,23 @@ const Navbar = () => {
   const currentUser = useSelector(state => state.currentUser.value);
   return (
     <nav className='flex flex-row md:flex-col bg-softDark shadow-md md:rounded-md overflow-hidden'>
-      <Link href="/dashboard/" className={`${pathname === "/dashboard" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full`}>
+      <Link href="/dashboard/" className={`${pathname === "/dashboard" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full order-1 md:order-none`}>
         <IoHome size={25}/>
         <span className="hidden md:block">Home</span>
       </Link>
-      <Link href="/dashboard/explore" className={`${pathname === "/dashboard/explore" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full`}>
+      <Link href="/dashboard/explore" className={`${pathname === "/dashboard/explore" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full order-2 md:order-none`}>
         <IoSearch size={25}/>
         <span className="hidden md:block">Explore</span>
       </Link>
-      <Link href="/dashboard/notifications" className={`${pathname === "/dashboard/notifications" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full`}>
+      <Link href="/dashboard/notifications" className={`${pathname === "/dashboard/notifications" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full order-5 md:order-none`}>
         <IoNotifications size={25}/>
         <span className="hidden md:block">Notifications</span>
       </Link>
-      <Link href="/dashboard/messages" className={`${pathname === "/dashboard/messages" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full`}>
+      <Link href="/dashboard/messages" className={`${pathname === "/dashboard/messages" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full order-4 md:order-none`}>
         <IoMail size={25}/>
         <span className="hidden md:block">Messages</span>
       </Link>
-      <Link href="/dashboard/friends" className={`${pathname === "/dashboard/friends" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full`}>
+      <Link href="/dashboard/friends" className={`${pathname === "/dashboard/friends" ? 'bg-green-500 text-black' : 'text-light'} hover:text-black hover:bg-green-500 font-medium p-4 flex justify-center md:justify-normal items-center gap-4 w-full order-3 md:order-none`}>
         <IoPeople size={25}/>
         <span className="hidden md:block">Friends</span>
       </Link>
