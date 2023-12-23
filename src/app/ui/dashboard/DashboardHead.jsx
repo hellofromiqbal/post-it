6035a82421f486db93f8577868d0f710e2213997
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-23 13:48:39 +0700

    adjust dashboard head component's z-index

--- src/app/ui/dashboard/DashboardHead.jsx
+++ src/app/ui/dashboard/DashboardHead.jsx
@@ -11,7 +11,7 @@ const DashboardHead = () => {
   const pathname = usePathname();
   const currentUser = useSelector(state => state.currentUser.value);
   return (
-    <div className='flex justify-between md:justify-center items-center p-4 md:p-0 sticky md:relative top-0 bg-softDark md:bg-transparent z-10 md:z-0 shadow-md md:shadow-none'>
+    <div className='flex justify-between md:justify-center items-center p-4 md:p-0 sticky md:relative top-0 bg-softDark md:bg-transparent z-20 md:z-0 shadow-md md:shadow-none'>
       <Link href={`/dashboard/profile/${currentUser?.username}`} className={`${pathname.includes(currentUser?.username) ? 'bg-green-500 text-black' : 'text-light'} flex md:hidden items-center gap-4 font-medium`}>
         <FaUserCircle className='w-[30px] md:w-[25px] h-[30px] md:h-[25px]'/>
         <span className="hidden md:block">Profile</span>
