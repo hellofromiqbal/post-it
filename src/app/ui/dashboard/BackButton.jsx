d91cbfdbfce01305bdfd14c7e3e267eb22d09046
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-26 19:51:43 +0700

    adjust back button functionality

--- src/app/ui/dashboard/BackButton.jsx
+++ src/app/ui/dashboard/BackButton.jsx
@@ -1,13 +1,19 @@
+"use client"
+
 import React from 'react';
-import Link from 'next/link';
 import { FaArrowLeft } from "react-icons/fa6";
+import { useRouter } from 'next/navigation';
 
 const BackButton = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.back();
+  };
   return (
-    <Link href="/dashboard" className='hidden md:flex gap-2 p-4 items-center opacity-70 w-max'>
+    <button className='hidden md:flex gap-2 p-4 items-center opacity-70 w-max' onClick={handleClick}>
       <FaArrowLeft size={15}/>
       <span>Back</span>
-    </Link>
+    </button>
   )
 };
 
