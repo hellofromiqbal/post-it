51c383c43e92920ef58979950651bf7a3d4940b6
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-26 13:16:29 +0700

    add staticData.js, adjust navbar & navlist component

--- src/app/ui/dashboard/Navbar.jsx
+++ src/app/ui/dashboard/Navbar.jsx
@@ -4,40 +4,20 @@ import React from 'react';
 import SignOutButton from './SignOutButton';
 import { useSelector } from 'react-redux';
 import NavLink from './NavLink';
+import staticData from '@/assets/data/staticData';
 
 const Navbar = () => {
   const currentUser = useSelector(state => state.currentUser.value);
   return (
     <nav className='flex flex-row md:flex-col bg-softDark shadow-md md:rounded-md overflow-hidden'>
-      <NavLink
-        name={'home'}
-        url={"/dashboard"}
-        mobileViewOrder={'order-1'}
-      />
-      <NavLink
-        name={'explore'}
-        url={"/dashboard/explore"}
-        mobileViewOrder={'order-2'}
-      />
-      <NavLink
-        name={'notifications'}
-        url={"/dashboard/notifications"}
-        mobileViewOrder={'order-5'}
-      />
-      <NavLink
-        name={'messages'}
-        url={"/dashboard/messages"}
-        mobileViewOrder={'order-4'}
-      />
-      <NavLink
-        name={'friends'}
-        url={"/dashboard/friends"}
-        mobileViewOrder={'order-3'}
-      />
-      <NavLink
-        name={'profile'}
-        url={`/dashboard/profile/${currentUser?.username}`}
-      />
+      {staticData.navbar.map((navLink) => (
+        <NavLink
+          key={navLink.name}
+          name={navLink.name}
+          url={navLink.name !== 'profile' ? navLink.url : navLink.url + `/${currentUser?.username}`}
+          mobileViewOrder={navLink.mobileViewOrder}
+        />
+      ))}
       <SignOutButton/>
     </nav>
   )
---
+++ src/assets/data/staticData.js
@@ -0,0 +1,12 @@
+const staticData = {
+  navbar: [
+    { name: 'home', url: "/dashboard", mobileViewOrder: 'order-1' },
+    { name: 'explore', url: "/dashboard/explore", mobileViewOrder: 'order-2' },
+    { name: 'notifications', url: "/dashboard/notifications", mobileViewOrder: 'order-5' },
+    { name: 'messages', url: "/dashboard/messages", mobileViewOrder: 'order-4' },
+    { name: 'friends', url: "/dashboard/friends", mobileViewOrder: 'order-3' },
+    { name: 'profile', url: "/dashboard/profile" },
+  ]
+};
+
+export default staticData;
