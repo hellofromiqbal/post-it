2d1fb7e42d5a372b83875ed767ffb6ac50ed08ad
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-26 13:52:55 +0700

    adjust trending list component & static data

--- src/app/ui/dashboard/TrendingList.jsx
+++ src/app/ui/dashboard/TrendingList.jsx
@@ -2,31 +2,19 @@
 
 import React from 'react';
 import Link from 'next/link';
+import staticData from '@/assets/data/staticData';
 
 const TrendingList = () => {
   return (
     <div className='bg-softDark shadow-md rounded-md text-light overflow-hidden flex flex-col'>
       <ul className='flex flex-col'>
-        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-          <h3 className='text-lg font-bold'>Free Palestine</h3>
-          <small className='text-xs font-semibold opacity-70'>31.3K posts</small>
-        </Link>
-        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-          <h3 className='text-lg font-bold'>Stop Genocide</h3>
-          <small className='text-xs font-semibold opacity-70'>29.1K posts</small>
-        </Link>
-        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-          <h3 className='text-lg font-bold'>Premier League</h3>
-          <small className='text-xs font-semibold opacity-70'>20.7K posts</small>
-        </Link>
-        <Link href="/dashboard/" className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
-          <small className='text-xs font-semibold opacity-70'>Trending in Norway</small>
-          <h3 className='text-lg font-bold'>Winter Is Coming</h3>
-          <small className='text-xs font-semibold opacity-70'>17.3K posts</small>
-        </Link>
+        {staticData.trendingList.map((trending) => (
+          <Link key={trending.title} href={trending.url} className='p-4 hover:bg-green-500 text-light hover:text-black flex flex-col'>
+            <small className='text-xs font-semibold opacity-70'>Trending in {trending.location}</small>
+            <h3 className='text-lg font-bold'>{trending.title}</h3>
+            <small className='text-xs font-semibold opacity-70'>{trending.postsQty}K posts</small>
+          </Link>
+        ))}
       </ul>
     </div>
   )
--- src/assets/data/staticData.js
+++ src/assets/data/staticData.js
@@ -6,6 +6,12 @@ const staticData = {
     { name: 'messages', url: "/dashboard/messages", mobileViewOrder: 'order-4' },
     { name: 'friends', url: "/dashboard/friends", mobileViewOrder: 'order-3' },
     { name: 'profile', url: "/dashboard/profile" },
+  ],
+  trendingList: [
+    { title: 'Free Palestine', url: "/dashboard", postsQty: 31.3, location: 'Norway' },
+    { title: 'Stop Genocide', url: "/dashboard", postsQty: 29.1, location: 'Germany' },
+    { title: 'Premier League', url: "/dashboard", postsQty: 20.7, location: 'England' },
+    { title: 'Winter Is Coming', url: "/dashboard", postsQty: 17.3, location: 'Norway' },
   ]
 };
 
