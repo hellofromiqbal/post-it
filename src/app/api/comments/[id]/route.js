05393aef4361db28877f1d635cbdaefe1a89a970
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-12-28 12:24:16 +0700

    adjust comment api route

--- src/app/api/comments/[id]/route.js
+++ src/app/api/comments/[id]/route.js
@@ -1,11 +1,15 @@
 import connectMongoDB from "@/libs/mongodb"
+import Like from "@/models/LikeModel";
 import Comment from "@/models/commentModel";
+import Post from "@/models/postModel";
 import { NextResponse } from 'next/server';
 
 // DELETE COMMENT
 export const DELETE = async (request, {params}) => {
   try {
     await connectMongoDB();
+    await Like.deleteMany({contentId: params.id});
+    await Comment.deleteMany({postId: params.id});
     await Comment.findByIdAndDelete(params.id);
     return NextResponse.json({
       success: true,
